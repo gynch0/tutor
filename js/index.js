2d3926731bde9614c83a9ef33b9ec21322fc320d
gynch0 <106382132+gynch0@users.noreply.github.com>
2026-04-06 21:34:44 +0400

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-/* modules */
-
-import './modules/back-to-top.js';
-import './modules/burger.js';
-import './modules/scroll-reveal.js';
-import './modules/faq.js';
-import './modules/hearts.js';
-import './modules/prices.js';
-import './modules/emoji-burst.js';
